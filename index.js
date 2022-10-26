d10fb38e80ea9d8cff97820d02e48163e5e46af1
Amy <86963506+aeposten@users.noreply.github.com>
2022-10-26 10:55:29 -0700

    Basic functionality works

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-import CHARACTERS from "./characters.js";
+// prettier-ignore
+const CHARACTERS  = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+    'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+    'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+    'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+    's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2',
+    '3', '4', '5', '6', '7', '8', '9', '~', '`', '!', '@',
+    '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+',
+    '=', '{', '[', '}', ']', ',', '|', ':', ';', '<', '>',
+    '.', '?', '/'
+  ]
+
+let passwordOne = document.getElementById("password-1");
+let passwordTwo = document.getElementById("password-2");
+
+function generatePassword(element, selectedLength) {
+  let passwordString = "";
+  for (let i = 0; i <= 15; i++) {
+    let randomIndex = Math.floor(Math.random() * CHARACTERS.length);
+    passwordString += CHARACTERS[randomIndex];
+  }
+    element.textContent = passwordString;
+}
 
